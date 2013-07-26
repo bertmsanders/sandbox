5dad633b5fc261b5f0947787847d4b8278aa6f83
sandaz13 <mike@sanders-tech.com>
2013-07-25 23:21:29 -0500

    Added stuff to mike scene

---
+++ MikeSand/SandyGame/Assets/MPlayerBehavior.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MPlayerBehavior : MonoBehaviour {
+	
+	public float moveSpeed;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Horizontal") > 0) {
+			rigidbody.AddForce(new Vector3(moveSpeed, 0f, 0f), ForceMode.VelocityChange);
+		} else if (Input.GetAxis("Horizontal") < 0) {
+			rigidbody.AddForce(new Vector3(-moveSpeed, 0f, 0f), ForceMode.VelocityChange);
+		}
+		
+		if (Input.GetAxis("Vertical") > 0) {
+			rigidbody.AddForce(new Vector3(0f, 0f, moveSpeed), ForceMode.VelocityChange);
+		} else if (Input.GetAxis("Vertical") < 0) {
+			rigidbody.AddForce(new Vector3(0f, 0f, -moveSpeed), ForceMode.VelocityChange);
+		}
+	}
+}
