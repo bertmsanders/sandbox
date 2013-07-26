d1585c5ac5bed5744fdd4b418430d658d5d34826
sandaz13 <mike@sanders-tech.com>
2013-07-26 00:41:32 -0500

    better jetpacks

---
+++ MikeSand/SandyGame/Assets/JetPackBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JetPackBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		renderer.material.color = Color.red;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Jump")) {
+			particleSystem.enableEmission = true;
+		}else{
+			particleSystem.enableEmission = false;
+		}
+	}
+}
--- MikeSand/SandyGame/Assets/MPlayerBehavior.cs
+++ MikeSand/SandyGame/Assets/MPlayerBehavior.cs
@@ -8,7 +8,7 @@ public class MPlayerBehavior : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-	
+		renderer.material.color = Color.black;
 	}
 	
 	// Update is called once per frame
